57f3b3f79b26e5bda5f3c19702ab873d667efb96
huangqiliang <huangqiliang@live.cn>
2026-07-18 16:31:29 +0800

    feat: add Windows setup instructions and fix command syntax
    - add Windows-specific installation section with python3 and pip3 commands
    - update clipboard_share script invocation to use python3
    - fix missing newline between pip uninstall and install commands

--- basic.sh
+++ basic.sh
@@ -1,11 +1,15 @@
 
 
 
+## Windows
+python3 clipboard_share_qt.py 
+pip3 install PySide6 -i https://pypi.tuna.tsinghua.edu.cn/simple
+
 
 
 python clipboard_share_qt.py --secret changeme
 
-
+pip uninstall -y PySide6 PySide6-Essentials PySide6-Addons shiboken6 pip install PySide6
 
 pip install PySide6 -i https://pypi.tuna.tsinghua.edu.cn/simple
 
